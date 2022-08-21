37cc69937778a2b4fe9e8c0d02afdb1912575705
WON <70957918+wondang99@users.noreply.github.com>
2022-08-21 18:31:06 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 96 ms, Memory: 32440 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -11,26 +11,20 @@ for n in range(N+M) :
     start.append(e[0])
     arrival.append(e[1])
 count = 0
-flag = 0
 
 route = deque([1])
-while flag==0:
+while deque:
     for now in range(len(route)) :
         step = route.popleft()
         for d in range(1,7) :
-            #step+=d 바보, step이 계속 쌓임
             nstep=step+d
             if nstep==100 :
-                break
+                print(count+1)
+                sys.exit(0)
             if nstep>100 or visited[nstep]:
                 continue
             if nstep in start :
                 nstep = arrival[start.index(nstep)]
             route.append(nstep)
             visited[nstep]=True
-        if nstep==100 :
-            flag=1
-            break
     count+=1
-        
-print(count)
